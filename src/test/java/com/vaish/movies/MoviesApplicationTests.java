99df96de35328a203ee30aef23f00b707ef0ac08
vaishnavisakhare03 <vaishnavisakhare0312@gmail.com>
2024-07-02 17:23:29 +0530

    project initial setup

---
+++ src/test/java/com/vaish/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaish.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
