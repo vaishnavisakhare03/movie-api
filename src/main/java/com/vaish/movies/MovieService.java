59966dace6b4c88358f81f16c645edbc5653d263
vaishnavisakhare03 <vaishnavisakhare0312@gmail.com>
2024-07-03 14:09:08 +0530

    completed api and mongodb connections

---
+++ src/main/java/com/vaish/movies/MovieService.java
@@ -0,0 +1,21 @@
+package com.vaish.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+    @Autowired
+    private MovieRepository movieRepository;
+    public List<Movie> allMovies(){
+        return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singleMovie(String imbdId){
+        return movieRepository.findMovieByImdbId(imbdId);
+    }
+}
---
+++ src/main/java/com/vaish/movies/ReviewRepository.java
@@ -0,0 +1,9 @@
+package com.vaish.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
